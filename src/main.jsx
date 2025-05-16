2ae798e7aaa2bfaae7e55e2de8240fbf0b3a45ef
Hadayetullah <hadayetullah30@gmail.com>
2025-05-16 15:39:42 +0600

    Create basic design for the google authentication

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+      </GoogleOAuthProvider>
   </React.StrictMode>,
 )
